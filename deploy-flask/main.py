9ec00881161a746e65a8c1dc79d25b1ff4c28de3
DAVIDPC\David <mercadodavidv@gmail.com>
2021-11-07 21:19:03 -0600

    added bluegill, channel catfish, and white bass to the model. note: the imagenet_class_index json actually needs the species listed in alphabetical order. should impove on this.

--- deploy-flask/main.py
+++ deploy-flask/main.py
@@ -9,13 +9,15 @@ import torch.nn as nn
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 app = Flask(__name__)
+
 imagenet_class_index = json.load(open('./imagenet_class_index.json'))
-model = torch.load('../model.pth')
-model.to('cpu')
+model = torch.load('./model.pth', map_location=torch.device('cpu'))
 model.eval()
 
 
@@ -30,34 +32,40 @@ def transform_image(image_bytes):
     return my_transforms(image).unsqueeze(0)
 
 
-def get_prediction(image_bytes, max_results=2):
+def get_prediction(image_bytes, num_results=4):
     tensor = transform_image(image_bytes=image_bytes)
     outputs = model.forward(tensor)
-    #_, y_hat = outputs.max(1)
-    #predicted_idx = str(y_hat.item())
-    #return imagenet_class_index[predicted_idx]
-    values, preds = torch.topk(outputs, max_results, dim=1)
-    sm = nn.functional.softmax(values, dim=1)
+    values, preds = torch.topk(outputs, num_results, dim=1)
+    confidence_level = nn.functional.softmax(values, dim=1)
     results = []
     percentages = []
-    for pred, perc in zip(preds[0], sm[0]):
+    for pred, perc in zip(preds[0], confidence_level[0]):
         percentages.append(perc)
         results.append(imagenet_class_index[str(pred.item())])
     return results, percentages
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"error": "No file in request body"}), 400
         file = request.files['file']
+        if file.filename == '' or not allowed_file(file.filename):
+            return jsonify({"error": "Invalid file format"}), 400
+
         img_bytes = file.read()
-        results, percentages = get_prediction(image_bytes=img_bytes)
+        results, percentages = get_prediction(image_bytes=img_bytes, num_results=4)
         jslist = [
             {'class_id': results[0][0], 'percentage': '{:.2f}'.format(percentages[0] * 100)},
-            {'class_id': results[1][0], 'percentage': '{:.2f}'.format(percentages[1] * 100)}
+            {'class_id': results[1][0], 'percentage': '{:.2f}'.format(percentages[1] * 100)},
+            {'class_id': results[2][0], 'percentage': '{:.2f}'.format(percentages[2] * 100)},
+            {'class_id': results[3][0], 'percentage': '{:.2f}'.format(percentages[3] * 100)}
         ]
         return json.dumps(jslist)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
